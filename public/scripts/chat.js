523593e9d3bcc430a59f8de39021be8946393819
NunoCartaxoUAL <81565338+NunoCartaxoUAL@users.noreply.github.com>
2023-02-21 01:35:34 +0000

    forgot to send chat.js , sent now

---
+++ public/scripts/chat.js
@@ -0,0 +1,37 @@
+let chatHistory;
+async function sendMessage(){
+    var msg = document.getElementById("talk").value
+    chatHistory += `\n${loggedUser}:`+msg+`.pet stats= [ health:${myPet.health}/100 ; food:${myPet.food}/200 ; water:${myPet.water}/100 ; mood:${myPet.mood}/100].\nPet:`;
+    addMessageToChat(msg)
+    document.getElementById("talk").value = ""
+    const response = await makeRequest("http://localhost:8080/chat", {
+        method: "POST",
+        body: JSON.stringify({ chatHistory,loggedUser,myPet }),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+    });
+    json = await response.json();
+    
+    addAnswerToChat(json.message)
+    chatHistory+=json.message
+    console.log(chatHistory)
+}
+function addAnswerToChat(msg){
+    document.getElementById("messages").innerHTML+=`
+    <section class="message -left">
+        <!-- Balloon -->
+        <div class="nes-balloon from-left" style="word-wrap: break-word; width: 95%;">
+            <p>${msg}</p>
+        </div>
+    </section>
+    `
+}
+function addMessageToChat(msg){
+    document.getElementById("messages").innerHTML+=`
+    <section class="message -right">
+        <!-- Balloon -->
+        <div class="nes-balloon from-right" style="word-wrap: break-word; width: 95%;">
+            <p>${msg}</p>
+        </div>
+    </section>
+    `
+}
